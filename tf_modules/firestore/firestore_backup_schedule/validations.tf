cb230b616e4c79c618aaf87a4741ac5f7abf1a7b
AHMED ZBYR <ahmedzbyr@gmail.com>
2023-10-19 15:51:53 +0100

    Adding comments

--- tf_modules/firestore/firestore_backup_schedule/validations.tf
+++ tf_modules/firestore/firestore_backup_schedule/validations.tf
@@ -1,8 +1,16 @@
+# Local variables to check the retention format based on the backup frequency.
 locals {
-  check_daily_backup_retention  = var.frequency == "DAILY" && substr(var.retention, -1, 1) == "d"
+  # Check if frequency is DAILY and retention format ends with "d".
+  check_daily_backup_retention = var.frequency == "DAILY" && substr(var.retention, -1, 1) == "d"
+
+  # Check if frequency is WEEKLY and retention format ends with "w".
   check_weekly_backup_retention = var.frequency == "WEEKLY" && substr(var.retention, -1, 1) == "w"
 }
 
+# A null resource that checks for the validity of the retention format.
 resource "null_resource" "check_rentention_is_valid" {
-  count = local.check_daily_backup_retention || local.check_weekly_backup_retention ? 0 : "ERROR: For DAILY backups use retention as `d` format (3d), for WEEKLY backups use rention as `w` format (3w)."
+  # Count is used to determine if the resource should be created or not.
+  # If either daily or weekly retention check passes, count will be 0 (resource will not be created).
+  # Otherwise, an error message is returned to inform the user of the correct format.
+  count = local.check_daily_backup_retention || local.check_weekly_backup_retention ? 0 : "ERROR: For DAILY backups use retention as `d` format (3d), for WEEKLY backups use retention as `w` format (3w)."
 }
